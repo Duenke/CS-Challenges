17069e02749369047e7585bc23cb5018db688e4d
Jacob Duenke <jacob.duenke@outlook.com>
2019-04-19 10:04:49 -0400

    Added dartboard Pi.

---
+++ DartboardPi/DartboardPi/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace DartboardPi
+{
+    internal class Program : Util
+    {
+        private static void Main(string[] args)
+        {
+            /*
+            Given the Dartboard Method of calculating Pi, write a program that throws random darts on a grid and calculate Pi.
+
+            Constraints:
+            - Number of darts will always be a positive integer above 10,000.
+
+            In-puts / Out-puts:
+            - Number of darts / ~3.14...
+
+            Logical Approach:
+            - Create a data structure with x,y values.
+            - Randomly populate the values.
+            - Determine how many of the random points are within a circle.
+            - Do the math.
+            */
+
+            Console.WriteLine("DartboardPi");
+            Console.WriteLine("Given the Dartboard Method of calculating Pi, write a program that throws random darts on a grid and calculate Pi.\n");
+
+            Console.Write("Enter the number of darts to throw at your circle on a grid: ");
+            int numDarts = int.Parse(Console.ReadLine());
+
+            Point[] points = new Point[numDarts];
+            points = PopulatePoints(points);
+
+            int numHits = CheckIfHit(points);
+            Console.WriteLine($"[{numHits}] of your darts hit within the circle!");
+
+            double Pi = CalcPi(numDarts, numHits);
+            Console.WriteLine($"Using the Dartboard Method, Pi is calculated to be ~[{Pi}]!");
+
+            Console.ReadKey();
+
+            Console.WriteLine("-----------------------------");
+        }
+    }
+}
---
+++ DartboardPi/DartboardPi/Util.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace DartboardPi
+{
+    public class Util
+    {
+        private static Random rnd = new Random();
+
+        public static Point[] PopulatePoints(Point[] points)
+        {
+            for (int i = 0; i < points.Length; i++)
+            {
+                points[i].x = rnd.NextDouble();
+                points[i].y = rnd.NextDouble();
+            }
+            return points;
+        }
+
+        public static int CheckIfHit(Point[] points)
+        {
+            int numHit = 0;
+            foreach (Point p in points)
+            {
+                if (Math.Sqrt((p.x * p.x) + (p.y * p.y)) < 1)
+                    numHit++;
+            }
+            return numHit;
+        }
+
+        public static double CalcPi(int numDarts, int numHits) => 4 * numHits / (double)numDarts;
+    }
+}
