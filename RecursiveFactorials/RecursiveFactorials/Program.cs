39e9414c8eb6d9e5ef8a13c67baf61d92714c7e7
Jacob Duenke <jacob.duenke@outlook.com>
2019-04-19 10:05:01 -0400

    Added RecursiveFactorials.

---
+++ RecursiveFactorials/RecursiveFactorials/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecursiveFactorials
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Write a recursive method to calculate factorial.");
+            int start = 1;
+            int acc = 1;
+            int end = 10;
+
+            int fac = GetFac(start, acc, end);
+            Console.WriteLine($"The factorial of {end} is {fac}.");
+        }
+
+        private static int GetFac(int start, int acc, int end)
+        {
+            Console.WriteLine($"{start}, {acc}, {end}");
+            start++;
+            acc *= start;
+            if (start < end)
+            {
+                acc = GetFac(start, acc, end);
+            }
+            return acc;
+        }
+    }
+}
