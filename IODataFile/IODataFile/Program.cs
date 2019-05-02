59499433ec46dc59cd8f274ec0350aa49f96535f
Jacob Duenke <jacob.duenke@outlook.com>
2019-05-02 12:37:12 -0400

    Added IODataFile.

---
+++ IODataFile/IODataFile/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IODataFile
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            string fileData = @"C:\Users\jacob\Downloads\imports-85.data";
+
+            string[] lines = File.ReadAllLines(fileData);
+            foreach (string line in lines)
+                Console.WriteLine(line);
+        }
+    }
+}
